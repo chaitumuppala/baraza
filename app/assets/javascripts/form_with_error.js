41001b959592e9d56d18dec46d30cc2e6a4ff19d
Deepthi <deepthirera@gmail.com>
2015-05-17 11:27:09 +0530

    Error messages style, js

--- app/assets/javascripts/form_with_error.js
+++ app/assets/javascripts/form_with_error.js
@@ -1,9 +1,12 @@
 $(function(){
     $('.reveal-modal form').each(function(i, form) {
+            var errorSection = $(form).find(".display_error");
         $(form).on('ajax:success',function(e, data, status, xhr){
+            errorSection.empty();
             location.reload();
         }).on('ajax:error',function(e, xhr, status, error){
-            $(form).find(".error").append( xhr.responseText );
+            errorSection.empty();
+            errorSection.append( xhr.responseText );
             $(form).parents(".reveal-modal").foundation('reveal', 'open');
         });
     });
