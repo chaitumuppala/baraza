6d1bc5c84e91072f39a5b23f9e78f4c970f3c2cd
Deepthi <deepthirera@gmail.com>
2015-06-01 12:49:31 +0530

    ckeditor image upload

---
+++ app/assets/javascripts/ckeditor/ck_config.js
@@ -0,0 +1,19 @@
+CKEDITOR.config.toolbarGroups = [
+    {name: 'clipboard', groups: ['clipboard', 'undo']},
+    {name: 'editing', groups: ['find', 'selection', 'spellchecker']},
+    {name: 'links'},
+    {name: 'insert'},
+    {name: 'forms'},
+    {name: 'tools'},
+    {name: 'document', groups: ['mode', 'document', 'doctools']},
+    {name: 'others'},
+    '/',
+    {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+    {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
+    {name: 'styles'},
+    {name: 'colors'},
+    {name: 'about'},
+    {name: 'mediaembed'}
+];
+
+CKEDITOR.config.format_tags = 'p;h1;h2;h3;pre';
