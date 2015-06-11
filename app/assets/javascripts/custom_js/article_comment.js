c9ca25328b2115595d4cf1b18ccd2f08059aa5d0
Deepthi <deepthirera@gmail.com>
2015-06-11 22:40:59 +0530

    Integrated disqus Comments

---
+++ app/assets/javascripts/custom_js/article_comment.js
@@ -0,0 +1,16 @@
+$(function () {
+  var ArticleComment = {
+    init: function () {
+      var disqus_shortname = 'barazademo';
+      /* * * DON'T EDIT BELOW THIS LINE * * */
+      (function () {
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    }
+  };
+  loadPageSpecificJs("articles", ['show'], ArticleComment.init.bind(ArticleComment));
+});
