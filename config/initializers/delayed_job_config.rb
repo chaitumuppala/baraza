4f423815e060710f3b39379c2481c1c0a21e7657
Deepthi <deepthirera@gmail.com>
2015-06-23 16:45:08 +0530

    dj | reduce logs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,2 +1,12 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+module Delayed
+  class Job
+    class << self
+      def reserve_with_log_silencer(worker, max_run_time = Worker.max_run_time)
+        Rails.logger.silence { reserve_without_log_silencer(worker, max_run_time) }
+      end
+      alias_method_chain :reserve, :log_silencer
+    end
+  end
+end
