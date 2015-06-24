2bca85520ef6a19d72d9a459ccc08825e8caeabd
Deepthi <deepthirera@gmail.com>
2015-06-24 16:08:04 +0530

    Success message in modal dialogs

--- app/assets/javascripts/custom_js/form_with_error.js
+++ app/assets/javascripts/custom_js/form_with_error.js
@@ -1,13 +1,19 @@
-$(function(){
-    $('.reveal-modal form').each(function(i, form) {
-        var errorSection = $(form).find(".display_error");
-        $(form).on('ajax:success',function(e, data, status, xhr){
-            errorSection.empty();
-            location.reload();
-        }).on('ajax:error',function(e, xhr, status, error){
-            errorSection.empty();
-            errorSection.append( xhr.responseText.split(", ").join(" <br> ") );
-            $(form).parents(".reveal-modal").foundation('reveal', 'open');
-        });
+$(function () {
+  $('.reveal-modal form').each(function (i, form) {
+    var errorSection = $(form).find(".display_error");
+    var submitButton = $(form).find("input[type=submit]");
+    var successFlash = $(form).parents(".reveal-modal").find(".success_flash");
+    $(form).on('ajax:success', function (e, data, status, xhr) {
+      submitButton.attr('disabled','disabled');
+      errorSection.empty();
+      successFlash.show();
+      setTimeout(function () {
+        location.reload();
+      }, 1000);
+    }).on('ajax:error', function (e, xhr, status, error) {
+      errorSection.empty();
+      errorSection.append(xhr.responseText.split(", ").join(" <br> "));
+      $(form).parents(".reveal-modal").foundation('reveal', 'open');
     });
+  });
 });
