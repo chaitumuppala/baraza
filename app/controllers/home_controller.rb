c6be16d42995df1438efa794aa6df0f463413b1f
Deepthi <deepthirera@gmail.com>
2015-07-14 18:05:12 +0530

    home_page_order | default to recently published

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,16 @@ class HomeController < ApplicationController
   filter_resource_access
 
   def index
-    @published_articles = Article.where(status: Article::Status::PUBLISHED).order(:home_page_order)
-    articles = @published_articles.select{ |article| article.home_page_order.present? }.group_by(&:home_page_order)
+    @published_articles = Article.where(status: Article::Status::PUBLISHED).order("date_published desc")
+    articles_with_order = @published_articles.order(:home_page_order).select{ |article| article.home_page_order.present? }.group_by(&:home_page_order)
+    articles_without_home_page_order = @published_articles.select{|article| article.home_page_order.blank?}
+    articles_without_home_page_order_index = 0
     @articles_with_order = (1..8).inject({}) do |result, order|
-      result[order] = articles[order].try(:first)
+      result[order] = articles_with_order[order].try(:first)
+      if result[order].blank?
+        result[order] = articles_without_home_page_order[articles_without_home_page_order_index]
+        articles_without_home_page_order_index+=1
+      end
       result
     end
   end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 describe HomeController do
-  it "should fetch published articles with home_page_order" do
+  it "should set blocks without home_page_order with recently published articles " do
     article1 = create(:article, home_page_order: 5, status: Article::Status::PUBLISHED)
     article2 = create(:article, home_page_order: 1, status: Article::Status::PUBLISHED)
     article3 = create(:article, home_page_order: 2, status: Article::Status::PUBLISHED)
     article4 = create(:article, home_page_order: nil, status: Article::Status::PUBLISHED)
+    article5 = create(:article, home_page_order: nil, status: Article::Status::PUBLISHED)
+    article1.update_attributes(date_published: Date.yesterday)
+    article2.update_attributes(date_published: 5.days.ago)
+    article3.update_attributes(date_published: 2.days.ago)
+    article4.update_attributes(date_published: Date.today)
     create(:article, home_page_order: 4, status: Article::Status::SUBMITTED_FOR_APPROVAL)
     create(:article, home_page_order: nil)
 
     get :index
 
-    expected_result = { 1 => article2, 2 => article3, 3 => nil, 4 => nil, 5 => article1, 6 => nil, 7 => nil, 8 => nil }
-    expect(assigns[:published_articles]).to eq([article4, article2, article3, article1])
+    expected_result = { 1 => article2, 2 => article3, 3 => article4, 4 => article5, 5 => article1, 6 => nil, 7 => nil, 8 => nil }
+    expect(assigns[:published_articles]).to eq([article4, article5, article1, article3, article2])
+    expect(assigns[:articles_with_order]).to eq(expected_result)
+  end
+
+  it "should by default show recently published articles if no article has home_page_order" do
+    article1 = create(:article, status: Article::Status::PUBLISHED)
+    article2 = create(:article, status: Article::Status::PUBLISHED)
+    article3 = create(:article, status: Article::Status::PUBLISHED)
+    article4 = create(:article, status: Article::Status::PUBLISHED)
+    article5 = create(:article, status: Article::Status::PUBLISHED)
+    article1.update_attributes(date_published: Date.yesterday)
+    article2.update_attributes(date_published: 5.days.ago)
+    article3.update_attributes(date_published: 2.days.ago)
+    article4.update_attributes(date_published: Date.today)
+    article5.update_attributes(date_published: Date.today)
+    create(:article, home_page_order: 4, status: Article::Status::SUBMITTED_FOR_APPROVAL)
+    create(:article, home_page_order: nil)
+
+    get :index
+
+    expected_result = { 1 => article4, 2 => article5, 3 => article1, 4 => article3, 5 => article2, 6 => nil, 7 => nil, 8 => nil }
     expect(assigns[:articles_with_order]).to eq(expected_result)
   end
 end
