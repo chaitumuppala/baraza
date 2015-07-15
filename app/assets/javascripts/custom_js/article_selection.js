7272fe1f01753ce844625fd2108c22c3836d443f
Deepthi <deepthirera@gmail.com>
2015-07-15 18:50:31 +0530

    foundation degraded since 5.5 is causing issues in chrome

--- app/assets/javascripts/custom_js/article_selection.js
+++ app/assets/javascripts/custom_js/article_selection.js
@@ -18,22 +18,10 @@ $(function () {
           that.setArticleOrder();
         }
       });
-
-      $("#admin-article .article").on('click', '.article-overlay', function (e) {
-        ArticleSelection.selectArticle($(e.target));
-      });
-    },
-
-    selectArticle: function(element) {
-      var article = element.parents(".article");
-      var articleImage = article.find("img");
-      var articleCheckBox = article.find("input[type=checkbox]");
-      article.toggleClass("chosen");
-      articleCheckBox.prop("checked", !articleCheckBox.prop("checked"));
     },
 
     setCategoryOrder: function() {
-      $("#category-sortable li").each(function(i, el){
+      $("#category-sortable dd").each(function(i, el){
         $(el).find(".cat_position").val($(el).index()+1);
       });
     },
