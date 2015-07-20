8fe9bf04cff65aad6cb60e0459b16452c181719d
Deepthi <deepthirera@gmail.com>
2015-07-20 16:42:28 +0530

    article_Form | js validation to validate file size

--- app/assets/javascripts/custom_js/article.js
+++ app/assets/javascripts/custom_js/article.js
@@ -3,6 +3,7 @@ $(function () {
     init: function () {
       this.initTags();
       $("#category-select").select2();
+      $(".articles form#article_form").on("submit", this.validateFile);
     },
 
     initTags: function() {
@@ -42,6 +43,37 @@ $(function () {
           }
         }
       });
+    },
+
+    validateFile: function(e) {
+      var form = $(e.target);
+      var errorDiv = form.find("#error_explanation");
+      errorDiv.find("ul").remove();
+      var file = $("#file_upload")[0].files[0];
+      var errorMessages = [];
+      var validSize = 2000000;
+      var validTypes = ["image/jpeg", "image/jpg", "image/png"];
+      var formValid = true;
+
+      if(file && file.size > validSize) {
+        errorMessages.push("<li> Cover image can't be greater than 2MB </li>");
+      }
+      if(file && !_.contains(validTypes, file.type)) {
+        errorMessages.push("<li> Cover image content type is invalid </li>");
+      }
+      if(CKEDITOR.instances.article_content.getData().length == 0) {
+        errorMessages.push("<li> Content can't be blank </li>");
+      }
+      if(errorMessages.length > 0) {
+        debugger;
+        var list = errorDiv.append('<ul></ul>').find('ul');
+        _.each(errorMessages, function(msg){
+          list.append(msg);
+        });
+        e.preventDefault();
+        formValid = false;
+      }
+      formValid;
     }
   };
   loadPageSpecificJs("articles", ['new', 'edit', 'create', 'update', 'approve_form', 'approve'], ArticleTag.init.bind(ArticleTag));
