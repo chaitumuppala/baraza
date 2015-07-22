4cecf88504fd1a234ae60a51ae930f6b2cc36092
Deepthi <deepthirera@gmail.com>
2015-07-22 17:58:53 +0530

    disabling ajax forms on submit

--- app/assets/javascripts/custom_js/form_with_error.js
+++ app/assets/javascripts/custom_js/form_with_error.js
@@ -3,8 +3,11 @@ $(function () {
     var errorSection = $(form).find(".display_error");
     var submitButton = $(form).find("input[type=submit]");
     var successFlash = $(form).parents(".reveal-modal").find(".success_flash");
-    $(form).on('ajax:success', function (e, data, status, xhr) {
+    $(form).on('submit', function() {
       submitButton.attr('disabled','disabled');
+    });
+
+    $(form).on('ajax:success', function (e, data, status, xhr) {
       errorSection.empty();
       successFlash.show();
       setTimeout(function () {
@@ -12,6 +15,7 @@ $(function () {
       }, 1000);
     }).on('ajax:error', function (e, xhr, status, error) {
       errorSection.empty();
+      submitButton.removeAttr('disabled');
       errorSection.append(xhr.responseText.split(", ").join(" <br> "));
       $(form).parents(".reveal-modal").foundation('reveal', 'open');
     });
