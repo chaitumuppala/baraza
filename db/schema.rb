211a2711885d2db98122885db5ed69892f56d92b
Deepthi <deepthirera@gmail.com>
2015-07-28 12:52:20 +0530

    article_categories removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728053128) do
+ActiveRecord::Schema.define(version: 20150728071959) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -27,16 +27,6 @@ ActiveRecord::Schema.define(version: 20150728053128) do
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-
-  create_table "article_categories", force: :cascade do |t|
-    t.integer  "article_id",  limit: 4
-    t.integer  "category_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "article_categories", ["article_id"], name: "index_article_categories_on_article_id", using: :btree
-  add_index "article_categories", ["category_id"], name: "index_article_categories_on_category_id", using: :btree
 
   create_table "article_tags", force: :cascade do |t|
     t.integer  "article_id", limit: 4
@@ -168,8 +158,6 @@ ActiveRecord::Schema.define(version: 20150728053128) do
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "article_categories", "articles"
-  add_foreign_key "article_categories", "categories"
   add_foreign_key "article_tags", "articles"
   add_foreign_key "article_tags", "tags"
   add_foreign_key "articles", "categories"
