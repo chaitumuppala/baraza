0a2345568b2eefe5703ab94968ea594ed5b9cb48
Deepthi <deepthirera@gmail.com>
2015-08-04 12:40:01 +0530

    z cover_image anf fb image changed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def cover_image_url_for(article)
-    url_to_image(article.cover_image.url(:medium))
+    url_to_image(article.cover_image.url)
   end
 end
--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -4,7 +4,7 @@ describe ArticlesHelper do
   context 'cover_image_url_for' do
     it 'should set the cover_image default image url if no image is provided' do
       article = create(:article)
-      expect(helper.cover_image_url_for(article)).to eq("http://test.host/assets/z_medium.png")
+      expect(helper.cover_image_url_for(article)).to eq("http://test.host/assets/z_original.png")
     end
 
     it 'should set the actual cover_image url, if present' do
