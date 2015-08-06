63e2b5a43e2ee88b4b7f9503ed6b0b88cc49194d
Deepthi <deepthirera@gmail.com>
2015-08-06 20:38:16 +0530

    Mailer subject changed on role change

--- app/mailers/type_change_notifier.rb
+++ app/mailers/type_change_notifier.rb
@@ -1,20 +1,20 @@
 class TypeChangeNotifier < ApplicationMailer
   def change_type_to_editor_mail(email, name, type)
-    change_type_mail(email, name, type)
+    change_type_mail(email, name, type, "You have been assigned as an editor on the Pan-African Baraza")
   end
 
   def change_type_to_administrator_mail(email, name, type)
-    change_type_mail(email, name, type)
+    change_type_mail(email, name, type, "You have been assigned as an admin on the Pan-African Baraza")
   end
 
   def change_type_to_registered_user_mail(email, name, type)
-    change_type_mail(email, name, type)
+    change_type_mail(email, name, type, "You have been assigned back to a registered user on the Pan-African Baraza")
   end
 
   private
-  def change_type_mail(email, name, type)
+  def change_type_mail(email, name, type, subject)
     @name = name
     @type = type
-    mail(to: email)
+    mail(to: email, subject: subject)
   end
 end
