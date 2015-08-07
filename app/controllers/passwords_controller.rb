cf32053c4d5c9ff6079a5005ab890f535ac61481
Deepthi <deepthirera@gmail.com>
2015-08-07 15:25:47 +0530

    show invalid on password reset

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,4 +11,15 @@ class PasswordsController < Devise::PasswordsController
       respond_with(resource)
     end
   end
+
+  def edit
+    reset_password_token = Devise.token_generator.digest(User, :reset_password_token, params[:reset_password_token])
+    self.resource = resource_class.find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
+    if resource.errors.empty?
+      self.resource = resource_class.new
+      resource.reset_password_token = params[:reset_password_token]
+    else
+      flash[:error] = "Reset password token is invalid"
+    end
+  end
 end
--- spec/controllers/passwords_controller_spec.rb
+++ spec/controllers/passwords_controller_spec.rb
@@ -18,4 +18,24 @@ describe PasswordsController do
       expect(response).to render_template("users/passwords/new")
     end
   end
+
+  context "edit" do
+    it "should render edit for valid token" do
+      user = create(:user)
+      token = user.send_reset_password_instructions
+      get :edit, reset_password_token: token
+
+      expect(response).to render_template("edit")
+      expect(flash[:error]).to be_nil
+    end
+
+    it "should render edit with error for invalid token" do
+      user = create(:user)
+      user.send_reset_password_instructions
+      get :edit, reset_password_token: "invalid token"
+
+      expect(response).to render_template("edit")
+      expect(flash[:error]).to eq("Reset password token is invalid")
+    end
+  end
 end
