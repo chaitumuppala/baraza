66e53891d6cda47b7e0eda5286d904236dd76638
Deepthi <deepthirera@gmail.com>
2015-08-12 11:57:18 +0530

    cap set linked files for env variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :bundle_flags, '--deployment'
 # Default value for :scm is :git
 set :scm, :git
 set :pty, true
-set :linked_dirs, %w{tmp/pids log}
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -26,10 +25,10 @@ set :linked_dirs, %w{tmp/pids log}
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push(".env.#{fetch(:stage)}")
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
