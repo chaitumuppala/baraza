b68b9d4f88ddd88f776bf06a3fefa6f5094fb2e1
Deepthi <deepthirera@gmail.com>
2015-08-24 11:13:30 +0530

    migration fix

--- db/migrate/20150820134110_change_from_user_id_to_creator_id.rb
+++ db/migrate/20150820134110_change_from_user_id_to_creator_id.rb
@@ -2,11 +2,9 @@ class ChangeFromUserIdToCreatorId < ActiveRecord::Migration
   def up
     remove_foreign_key :articles, :user
     rename_column :articles, :user_id, :creator_id
-    add_foreign_key :articles, :creator
   end
 
   def down
-    remove_foreign_key :articles, :creator
     rename_column :articles, :creator_id, :user_id
     add_foreign_key :articles, :user
   end
