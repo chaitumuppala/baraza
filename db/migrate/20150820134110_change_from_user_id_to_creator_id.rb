e98270fa593476ce74e4b3296f354b81c73d0de7
Deepthi <deepthirera@gmail.com>
2015-08-24 10:38:06 +0530

    mysql foreign_key error

--- db/migrate/20150820134110_change_from_user_id_to_creator_id.rb
+++ db/migrate/20150820134110_change_from_user_id_to_creator_id.rb
@@ -1,5 +1,13 @@
 class ChangeFromUserIdToCreatorId < ActiveRecord::Migration
-  def change
+  def up
+    remove_foreign_key :articles, :user
     rename_column :articles, :user_id, :creator_id
+    add_foreign_key :articles, :creator
+  end
+
+  def down
+    remove_foreign_key :articles, :creator
+    rename_column :articles, :creator_id, :user_id
+    add_foreign_key :articles, :user
   end
 end
