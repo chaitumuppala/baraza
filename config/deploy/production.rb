b88da252440d9036da677854946ba0aeb3ea650f
Deepthi <deepthirera@gmail.com>
2015-08-31 19:54:04 +0530

    bullet fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,14 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+server '78.47.156.19',
+       user: 'deploy',
+       roles: %w{web},
+       ssh_options: {
+           keys: File.join(ENV["HOME"], '.ssh', 'p_deploy_user_key.cer'),
+           forward_agent: false,
+           auth_methods: %w{publickey}
+       }
 
 
 # role-based syntax
