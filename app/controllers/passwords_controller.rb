21c028798c17d234cac898ec464514d1a03d2218
Deepthi <deepthirera@gmail.com>
2015-09-18 16:45:30 +0530

    password_controller spec fix; removed create method since parent create is enough

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,17 +1,4 @@
 class PasswordsController < Devise::PasswordsController
-  def create
-    self.resource = resource_class.where(email: params[:user][:email]).first
-    # render "users/passwords/new" and return if self.resource.try(:has_provider?)
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    yield resource if block_given?
-
-    if successfully_sent?(resource)
-      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
-
   def edit
     reset_password_token = Devise.token_generator.digest(User, :reset_password_token, params[:reset_password_token])
     self.resource = resource_class.find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
