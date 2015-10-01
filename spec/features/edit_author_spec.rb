a6041e9f4c068f4153a0d5460d3f7cf4d4706039
Mmathabo <mmathabo@Mmathabos-MacBook-Pro.local>
2015-10-01 12:47:59 +0200

    [Mmathabo, Hans] fixing the build

--- spec/features/edit_author_spec.rb
+++ spec/features/edit_author_spec.rb
@@ -3,12 +3,16 @@ require 'rails_ui_helper'
 describe 'edit author details' do
 
   before :each do
-    @login_page.login
+    @password = "Password1!"
+    @user = create :user, password: @password, type:'Administrator'
+    @login_page.login(@user.email , @password)
     @author_name = 'Orville Khangale'
     @author_email = "ok@gmail.com"
   end
 
   it 'Edit author details from home page' do
+    @home_page.home
+    @home_page.click_username
     @add_author_page.go_to_authors
     @add_author_page.enter_author_details(@author_name, @author_email)
     @add_author_page.go_to_authors
