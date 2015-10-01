4c957e501a2e822303f025e60dc1148e2969f628
Mmathabo <mmathabo@Mmathabos-MacBook-Pro.local>
2015-10-01 12:33:53 +0200

    [Mmathabo] Fixing the build

--- spec/features/add_author_spec.rb
+++ spec/features/add_author_spec.rb
@@ -3,18 +3,19 @@ require 'rails_ui_helper'
 describe 'add author' do
   before :each do
     @password = "Password1!"
-    @user = create :user, password: @password
+    @user = create :user, password: @password, type:'Administrator'
     @article = create :article, creator: @user
     @article.users << @user
   end
 
   it 'should add author' do
-    @login_page.login("email3@factory.com" , @password)
-    #expect(page).to have_content("Signed in successfully")
+
+    @login_page.login(@user.email , @password)
+    expect(page).to have_content("Signed in successfully")
     @home_page.home
     @home_page.click_username
     @add_author_page.go_to_authors
     @add_author_page.enter_author_details("Hans M","hm@mail.com")
-    #expect(page).to have_content("Author was successfully created.")
+    expect(page).to have_content("Author was successfully created.")
   end
 end
