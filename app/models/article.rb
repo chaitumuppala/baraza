405e4d9fc28c5628f332dd36bf5173264dca157b
Brett Strydom <bstrydo@thoughtworks.com>
2015-10-07 12:37:09 +0200

    fixed a bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -29,8 +29,8 @@ class Article < ActiveRecord::Base
   has_many :article_owners, :dependent => :destroy
   has_many :system_users, through: :article_owners, source: :owner, source_type: Author.name
   has_many :users, through: :article_owners, source: :owner, source_type: User.name
-  has_one :cover_image, -> { where(preview_image: false) }
-  has_and_belongs_to_many :tags
+  has_one :cover_image, -> { where(preview_image: false) }, :dependent => :destroy
+  has_and_belongs_to_many :tags, :dependent => :destroy
   belongs_to :category
 
   acts_as_taggable_on :tags
