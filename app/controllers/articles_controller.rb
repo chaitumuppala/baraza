cebf5e6ea043aefbc4a0a176789309dd1d64ffd7
Mmathabo <mmathabo@Mmathabos-MacBook-Pro.local>
2015-10-22 11:09:16 +0200

    #121 seperating draft application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -113,7 +113,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = current_user.articles.includes(:users, :system_users).where('status != ?',  Article::Status::PREVIEW)
+    @articles = current_user.articles.includes(:users, :system_users).where('status != ?',  Article::Status::SUBMITTED_FOR_APPROVAL)
     @proxy_articles = current_user.proxy_articles.includes(:users, :system_users).where('status != ?', Article::Status::PREVIEW) - @articles
     @articles_submitted = Article.where(status: Article::Status::SUBMITTED_FOR_APPROVAL).includes(:users, :system_users) unless current_user.registered_user?
     @articles_by_others = Article.where(status: Article::Status::PUBLISHED).includes(:users, :system_users) unless current_user.registered_user?
